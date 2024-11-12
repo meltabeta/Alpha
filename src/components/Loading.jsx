42a6e26d1ab46b28f5c1d3eb7c2de5488ca07894
seang_sengly <seangsengly0001@gmail.com>
2024-11-13 00:32:23 +0700

    improve loading to center screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ function Loading() {
     <div className="loading-wrapper">
       <div className="loading-container">
         <div className="loading-spinner"></div>
-        <span className="loading-text">Loading</span>
+        <span className="loading-text">Loading...</span>
       </div>
     </div>
   )
